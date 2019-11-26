c3b7d8edb566f4b5357790f111e301fbf6d6e176
Adil Reza <adilreza043@gmail.com>
2019-11-26 23:14:36 +0600

    learning about list

---
+++ dart_list.dart
@@ -0,0 +1,18 @@
+void main()
+{
+  print("--------all about dart list ------");
+  var list1 = new List(10);
+  int i;
+  list1[0]="adil";
+  list1[1]="reza";
+  list1[2]=12;
+  list1[3]=456.77;
+  for(i=0;i<4;i++)
+  {
+    print(list1[i]);
+  }
+
+
+}
+
+
